1dbf62616f7373b23e1f3228818673cdbb5ccdc3
CelsoTNeto <cteixeiraneto@hotmail.com>
2019-04-17 22:52:10 -0300

    Tela com Lista Array

--- ProjetoAndroid-master/TelaCadastro/app/src/main/java/com/example/aluno/telacadastro/tela_Array.kt
+++ ProjetoAndroid-master/TelaCadastro/app/src/main/java/com/example/aluno/telacadastro/tela_Array.kt
@@ -2,6 +2,8 @@ package com.example.aluno.telacadastro
 
 import android.os.Bundle
 import android.app.Activity
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 import kotlinx.android.synthetic.main.activity_tela__array.*
 
@@ -10,6 +12,15 @@ class tela_Array : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela__array)
-    }
 
+
+        var listView = findViewById<ListView>(R.id.listview1)
+        val arrayDeLinhas: ArrayList<String> = ArrayList<String>()
+        arrayDeLinhas.add("Primeira Linha ...")
+        arrayDeLinhas.add("Segunda Linha ...")
+        arrayDeLinhas.add("Terceira Linha ...")
+        arrayDeLinhas.add("Quarta Linha ...")
+        arrayDeLinhas.add("Quinta Linha ...")
+        listView.adapter = ArrayAdapter(this,android.R.layout.simple_expandable_list_item_1, arrayDeLinhas)
+    }
 }
