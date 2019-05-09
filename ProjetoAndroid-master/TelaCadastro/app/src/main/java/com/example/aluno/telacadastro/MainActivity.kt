657c2744b1b6422c0c889cfbfa7105e471553c6e
CelsoTNeto <cteixeiraneto@hotmail.com>
2019-05-09 08:02:26 -0300

    Inserindo banco de dados

--- ProjetoAndroid-master/TelaCadastro/app/src/main/java/com/example/aluno/telacadastro/MainActivity.kt
+++ ProjetoAndroid-master/TelaCadastro/app/src/main/java/com/example/aluno/telacadastro/MainActivity.kt
@@ -8,23 +8,55 @@ import android.widget.TextView
 import com.example.aluno.telacadastro.R.id.*
 import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.*
 import org.jetbrains.anko.custom.ankoView
-import org.jetbrains.anko.startActivity
-
-import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+      val btnArray: Button = findViewById<Button>(R.id.btnArray)
+        val btnincluir: Button = findViewById<Button>(R.id.btnincluir)
+        val btnlimpar: Button = findViewById<Button>(R.id.btnlimpar)
+        val btnverificar: Button = findViewById<Button>(R.id.btnverificar)
 
-    val btnincluir : Button = findViewById<Button>(R.id.btnincluir)
-        val btnlimpar : Button = findViewById<Button>(R.id.btnlimpar)
-        val btnArray : Button = findViewById<Button>(R.id.btnArray)
+
+
+
+
 
         btnincluir.setOnClickListener(View.OnClickListener {
+            var u: Usuario = Usuario(
+                   txtnome.text.toString(),
+                    txtemail.text.toString(),
+                    txtlogin.text.toString(),
+                    txtsenha.text.toString()
+
+            )
+            UsuarioRepository(getApplicationContext()).create(u)
+
+            alert(
+                    "Usuario Cadastrado com Sucesso",
+                    "Vc ja conhecia o toast com maior duracao?") {
+                yesButton { longToast("Esse toast dura mais tempo…") }
+                noButton {}
+            }.show()
+
+            toast("toast normal: Usuario Incluido com sucesso")
         })
+
+        btnverificar.setOnClickListener(View.OnClickListener {
+            toast("Listar Usuarios...")
+            var listU: List<Usuario> = UsuarioRepository(getApplicationContext()).findAll()
+            for (u: Usuario in listU) {
+                toast(u.nome)
+            }
+        })
+
+
+
+
 
         btnlimpar.setOnClickListener(View.OnClickListener {
             val txtnome = txtnome.text.clear()
@@ -41,3 +73,4 @@ class MainActivity : AppCompatActivity() {
 
     }
 }
+
